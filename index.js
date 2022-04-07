9ed049951544a508c4e44b39c9174ea173f084f3
Md Abu Naser Kayes <abunaserkayes0@gmail.com>
2022-04-07 23:45:10 +0600

    finish

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function updateAmount(amount) {
   const amountValue = parseFloat(amountText.value);
   return amountValue;
 }
-
+// Incomes and Expenses Calculation section
 document.getElementById("calculate-btn").addEventListener("click", function () {
   const incomeAmount = updateAmount("income-amount");
   const foodAmount = updateAmount("food-amount");
@@ -24,16 +24,20 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
     document.getElementById("error-message").style.display = "none";
   }
 });
-
+// Saving Calculation section
 document.getElementById("save-btn").addEventListener("click", function () {
-  const saveRate = updateAmount("save-rate");
-  const incomeAmount = updateAmount("income-amount");
-
-  const saveAmount = (incomeAmount * saveRate) / 100;
-  document.getElementById("save-amount").innerText = saveAmount;
+  const saveRate = document.getElementById("save-rate").value;
+  const incomeAmount = document.getElementById("income-amount").value;
 
   const balanceAmount = document.getElementById("rest-amount");
   const balanceAmountText = parseFloat(balanceAmount.innerText);
+  const saveAmount = (incomeAmount * saveRate) / 100;
+  if (balanceAmountText < saveAmount) {
+    document.getElementById("error-items").style.display = "block";
+    return;
+  }
+  document.getElementById("save-amount").innerText = saveAmount;
   const remainingBalance = balanceAmountText - saveAmount;
   document.getElementById("remaining-amount").innerText = remainingBalance;
+  document.getElementById("error-items").style.display = "none";
 });
