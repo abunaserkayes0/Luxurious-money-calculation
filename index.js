958759c6d730679a931ee0ff5e70b0004b984680
Md Abu Naser Kayes <abunaserkayes0@gmail.com>
2022-04-07 23:10:46 +0600

    update-functional-components

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+function updateAmount(amount) {
+  const amountText = document.getElementById(amount);
+  const amountValue = parseFloat(amountText.value);
+  return amountValue;
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const incomeAmount = document.getElementById("income-amount");
-  const incomeAmountValue = parseFloat(incomeAmount.value);
+  const incomeAmount = updateAmount("income-amount");
+  const foodAmount = updateAmount("food-amount");
+  const rentAmount = updateAmount("rent-amount");
+  const clothesAmount = updateAmount("clothes-amount");
 
-  const foodAmount = document.getElementById("food-amount");
-  const foodAmountValue = parseFloat(foodAmount.value);
-  const rentAmount = document.getElementById("rent-amount");
-  const rentAmountValue = parseFloat(rentAmount.value);
-  const clothAmount = document.getElementById("clothes-amount");
-  const clothAmountValue = parseFloat(clothAmount.value);
-
-  const totalAmount = foodAmountValue + rentAmountValue + clothAmountValue;
+  const totalAmount = foodAmount + rentAmount + clothesAmount;
 
   const totalExpenses = document.getElementById("total-expenses");
   totalExpenses.innerText = totalAmount;
 
-  if (incomeAmountValue < totalAmount) {
+  if (incomeAmount < totalAmount) {
     document.getElementById("error-message").style.display = "block";
   } else {
     const restAmount = document.getElementById("rest-amount");
-    const restBalance = incomeAmountValue - totalAmount;
+    const restBalance = incomeAmount - totalAmount;
     restAmount.innerText = restBalance;
+    document.getElementById("error-message").style.display = "none";
   }
+});
+
+document.getElementById("save-btn").addEventListener("click", function () {
+  const saveRate = updateAmount("save-rate");
+  const incomeAmount = updateAmount("income-amount");
+
+  const saveAmount = (incomeAmount * saveRate) / 100;
+  document.getElementById("save-amount").innerText = saveAmount;
+
+  const balanceAmount = document.getElementById("rest-amount");
+  const balanceAmountText = parseFloat(balanceAmount.innerText);
+  const remainingBalance = balanceAmountText - saveAmount;
+  document.getElementById("remaining-amount").innerText = remainingBalance;
 });
