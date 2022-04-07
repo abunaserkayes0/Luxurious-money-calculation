dc30bf8ca906941e3243ac86b76129e39a843e70
Md Abu Naser Kayes <abunaserkayes0@gmail.com>
2022-04-07 22:17:26 +0600

    update-total

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const incomeAmount = document.getElementById("income-amount");
+  const incomeAmountValue = parseFloat(incomeAmount.value);
+
+  const foodAmount = document.getElementById("food-amount");
+  const foodAmountValue = parseFloat(foodAmount.value);
+  const rentAmount = document.getElementById("rent-amount");
+  const rentAmountValue = parseFloat(rentAmount.value);
+  const clothAmount = document.getElementById("clothes-amount");
+  const clothAmountValue = parseFloat(clothAmount.value);
+
+  const totalAmount = foodAmountValue + rentAmountValue + clothAmountValue;
+
+  const totalExpenses = document.getElementById("total-expenses");
+  totalExpenses.innerText = totalAmount;
+
+});
